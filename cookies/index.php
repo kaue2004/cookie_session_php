57b833c1106444ee16a943e45c1aa90d8456d151
Pablo Kauê <54450039+kaue2004@users.noreply.github.com>
2020-03-20 21:37:01 -0300

    exercício 2

---
+++ cookies/index.php
@@ -0,0 +1,12 @@
+<form action="" method="post"> 
+    <input type="text" name="" $user>
+     <input type="password" name="" $login> 
+     <button>Salvar dados</button>
+      </form> 
+  <?php 
+  $user;
+  $login; 
+  setcookie("user");
+  setcookie("login"); 
+  ?> 
+  
