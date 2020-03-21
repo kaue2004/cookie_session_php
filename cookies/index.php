705bbc61419c9bb8c7440fc02d0695cae1f47769
Pablo Kauê <54450039+kaue2004@users.noreply.github.com>
2020-03-21 10:47:32 -0300

    Exercício 1

--- cookies/index.php
+++ cookies/index.php
@@ -1,12 +1,19 @@
-<form action="" method="post"> 
-    <input type="text" name="" $user>
-     <input type="password" name="" $login> 
-     <button>Salvar dados em cookie</button>
-      </form> 
-  <?php 
-  $user;
-  $login; 
-  setcookie("user");
-  setcookie("login"); 
-  ?> 
-  
+<?php
+if(isset($_COOKIE['nome']) && isset($_GET['deletecookie'])){
+    unset($_COOKIE['nome']);
+    echo '<script>alert("cookie deletado")
+    </script>'; 
+} if(isset($_POST['user'])){
+    setcookie('nome', $_POST['user'], time()+60*60*24*30);
+    header("location: index.php");
+}else{ 
+    if(isset($_COOKIE['nome'])){
+        echo $_COOKIE['nome']; 
+        echo "<a href='?deletecookie=true'><button>Deletar Cookie</button></a>"; 
+    }else{ 
+?>
+<form method="POST" action="index.php">
+    <input type="text" id="user" name="user" placeholder="Nome"><br>
+    <input type="submit" value="Enviar">
+</form>
+<?php } } ?>
